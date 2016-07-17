a167252748c2412fd8e28a4da6ec45fcc4a89225
Andrei Darien Elliott <a.d.elliott@ku.edu>
2016-07-17 18:54:51 -0500

    inital support for binding identifiers

--- Core.hs
+++ Core.hs
@@ -12,7 +12,7 @@ import Control.Monad (mzero)
 
 import State
 
-data Primative = (:+) | (:-) | Print
+data Primative = (:+) | (:-) | Print | Bind
                deriving Show
 
 data Symbol :: * where
@@ -20,6 +20,7 @@ data Symbol :: * where
      StrLit :: String -> Symbol
      PrimOp :: Primative -> Symbol
      Quotation :: Prgm -> Symbol
+     Name :: String -> Symbol
      deriving Show
 
 type Prgm = [Symbol]
@@ -28,48 +29,64 @@ data Expr :: * where
      MInt :: Integer -> Expr
      MString :: String -> Expr
      MVoid :: Expr
+     MName :: Name -> Expr
 
 instance Show Expr where
     show (MInt n) = show n
     show (MString s) = show s
     show MVoid = "()"
 
+type Name = String
+
 type Stack = [Expr]
 
 showStack :: Stack -> String
 showStack = unwords . map show . reverse
 
+-- may want to replace later (Data.Map maybe?)
+type Bindings = [(Name, Expr)]
+
+insert :: Name -> Expr -> Bindings -> Bindings
+insert name value = ((name, value):)
+
 data MState :: * where
-     MState :: IO () -> Stack -> MState
+     MState :: IO () -> Stack -> Bindings -> MState
 
 instance Monoid MState where
-    mempty = MState (return ()) []
-    (MState a0 s0) `mappend` (MState a1 s1) = MState (a0 >> a1) (s1 ++ s0)
+    mempty = MState (return ()) [] []
+    (MState a0 s0 b0) `mappend` (MState a1 s1 b1) = MState (a0 >> a1) (s1 ++ s0) (b0 `mappend` b1)
 
 stack :: MState -> Stack
-stack (MState _ s) = s
+stack (MState _ s _) = s
 
 action :: MState -> IO ()
-action (MState a _) = a
+action (MState a _ _) = a
+
+bindings :: MState -> Bindings
+bindings (MState _ _ b) = b
 
 setAction :: MState -> IO() -> MState
-setAction s a = MState a (stack s)
+setAction st a = MState a (stack st) (bindings st)
 
 appendAction :: IO () -> State MState Expr
-appendAction act = State $ \(MState a s) -> (MVoid, MState (a >> act) s)
+appendAction act = State $ \(MState a s b) -> (MVoid, MState (a >> act) s b)
+
+addBinding :: Name -> Expr -> State MState Expr
+addBinding name value = State $ \s -> (value, MState (action s) (stack s) (insert name value (bindings s)))
 
 push :: Expr -> State MState Expr
-push e = State $ \s -> (MVoid, MState (action s) (e:stack s))
+push e = State $ \s -> (MVoid, MState (action s) (e:stack s) (bindings s))
 
 pop :: State MState Expr
 pop = State $ \s -> let l = stack s
-                    in (head l, MState (action s) (tail l))
+                    in (head l, MState (action s) (tail l) (bindings s))
 
 
 step :: Symbol -> State MState Expr
 step (IntLit n) = push (MInt n)
 step (StrLit s) = push (MString s)
 step (PrimOp o) = stepPrim o
+step (Name s)   = push (MName s)
 
 stepPrim :: Primative -> State MState Expr
 stepPrim (:+) = do MInt x <- pop
@@ -85,7 +102,10 @@ stepPrim (:-) = do MInt x <- pop
 stepPrim Print = do p <- pop
                     appendAction (print p)
                     return p
-
+stepPrim Bind = do MName nm <- pop
+                   expr <- pop
+                   addBinding nm expr
+                   return expr
 
 run :: Prgm -> (Expr, MState)
 run = flip runEnv mempty
