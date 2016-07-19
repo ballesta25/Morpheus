b844ffaa4e5daf35ac125f892b337255eca2ecab
Andrei Darien Elliott <a.d.elliott@ku.edu>
2016-07-19 00:01:47 -0500

    Core now supports basic binding of values to names (we have variables now).  Still need to finalize a syntax and teach the parser about them.

--- Core.hs
+++ Core.hs
@@ -20,7 +20,8 @@ data Symbol :: * where
      StrLit :: String -> Symbol
      PrimOp :: Primative -> Symbol
      Quotation :: Prgm -> Symbol
-     Name :: String -> Symbol
+     QuotID :: Name -> Symbol
+     Identifier :: Name -> Symbol
      deriving Show
 
 type Prgm = [Symbol]
@@ -35,6 +36,7 @@ instance Show Expr where
     show (MInt n) = show n
     show (MString s) = show s
     show MVoid = "()"
+    show (MName nm) = '\'':nm
 
 type Name = String
 
@@ -74,8 +76,11 @@ appendAction act = State $ \(MState a s b) -> (MVoid, MState (a >> act) s b)
 addBinding :: Name -> Expr -> State MState Expr
 addBinding name value = State $ \s -> (value, MState (action s) (stack s) (insert name value (bindings s)))
 
+lookupBinding :: Name -> State MState (Maybe Expr)
+lookupBinding nm = State $ \s -> (lookup nm (bindings s), s)
+
 push :: Expr -> State MState Expr
-push e = State $ \s -> (MVoid, MState (action s) (e:stack s) (bindings s))
+push e = State $ \s -> (e, MState (action s) (e:stack s) (bindings s))
 
 pop :: State MState Expr
 pop = State $ \s -> let l = stack s
@@ -86,19 +91,18 @@ step :: Symbol -> State MState Expr
 step (IntLit n) = push (MInt n)
 step (StrLit s) = push (MString s)
 step (PrimOp o) = stepPrim o
-step (Name s)   = push (MName s)
+step (QuotID s) = push (MName s)
+step (Identifier s) = runIdentifier s
 
 stepPrim :: Primative -> State MState Expr
 stepPrim (:+) = do MInt x <- pop
                    MInt y <- pop
                    let res = MInt $ y + x
                    push res
-                   return res
 stepPrim (:-) = do MInt x <- pop
                    MInt y <- pop
                    let res = MInt $ y - x
                    push res
-                   return res
 stepPrim Print = do p <- pop
                     appendAction (print p)
                     return p
@@ -106,6 +110,11 @@ stepPrim Bind = do MName nm <- pop
                    expr <- pop
                    addBinding nm expr
                    return expr
+
+-- this will need to become more sophisticated to deal with morphemes
+runIdentifier :: Name -> State MState Expr
+runIdentifier nm = do Just value <- lookupBinding nm
+                      push value
 
 run :: Prgm -> (Expr, MState)
 run = flip runEnv mempty
