f0d3b0a2d6f5cb52669c45bbe2809b6c299369eb
ballesta25 <ballesta25@gmail.com>
2017-07-19 17:31:52 -0500

    can now parse parenthesized program fragments

--- Parser.hs
+++ Parser.hs
@@ -1,28 +1,58 @@
 {-
 
 -}
-module Parser (parse, parseSymbol) where
+
+{-# LANGUAGE GADTs, KindSignatures #-}
+
+module Parser (parse, lexSymbol) where
 import Text.Read  (readMaybe)
 import Data.Maybe (isJust)
 import Data.Char
 
 import Core
 
+data Token :: * where
+  Bracket  :: Direction -> Paren -> Token
+  Tok      :: Symbol -> Token
+  deriving Show
+
+tokenize :: String -> [String]
+tokenize = words -- may want to update
+
 -- returns either the parsed program, or a list of errors
 parse :: String -> Either [String] Prgm
-parse = accumulateParse . map parseSymbol . words
+parse = accumulateParse . map lexSymbol . tokenize
 
-accumulateParse :: [Either String Symbol] -> Either [String] Prgm
-accumulateParse [] = Right []
-accumulateParse (Right s : xs) = case accumulateParse xs of
-                                   Right ys -> Right (s:ys)
+accumulateParse :: [Either String Token] -> Either [String] Prgm
+accumulateParse = clean . acc []
+  where clean (Left x)    = Left x
+        clean (Right [])  = Right [] -- empty program
+        clean (Right [x]) = Right x
+        clean _ = Left ["error in clean step."]
+
+acc :: [Paren] -> [Either String Token] -> Either [String] [Prgm]
+acc [] [] = Right [[]]
+acc bt [] = Left ["reached end with unmatched parens: " ++ show bt]
+acc bt (Right (Tok s) : xs) = case acc bt xs of
+                                   Right (ys : zss) -> Right ((s:ys) : zss)
                                    errs -> errs
-accumulateParse (Left err : xs) = Left $ err : foldr filterErr [] xs
+acc bt (Right (Bracket L b) : xs) = case acc (b : bt) xs of
+  Right (exp : rest : zss) -> Right $ (Quotation exp : rest) : zss
+  errs -> errs
+acc [] (Right (Bracket R b) : xs) = acc [] $ (Left $ "unmatched closing paren: " ++ show b) : xs
+acc (p : bt) (Right (Bracket R b) : xs) = if p == b
+                                          then
+                                            case acc bt xs of
+                                              Right ys -> Right ([] : ys)
+                                              errs -> errs
+                                          else
+                                            acc bt $ (Left $ "mismatched parens: expected: " ++ show p ++ " found: " ++ show b) : xs
+acc bt (Left err : xs) = Left $ err : foldr filterErr [] xs
     where filterErr (Left e) = (e:)
           filterErr _ = id
 
-parseSymbol :: String -> Either String Symbol
-parseSymbol str = case readSymbol str of
+lexSymbol :: String -> Either String Token
+lexSymbol str = case readSymbol str of
                     Just s -> Right s
                     Nothing -> Left $ "Couldn't read: " ++ str
 
@@ -54,8 +84,21 @@ readIdentifier s = if all isAlphaNum s  -- TODO: fix to allow morpheme decomposi
                    then Just s
                    else Nothing
 
-readSymbol :: String -> Maybe Symbol
+
+-- add more: maybe infinite possibilities like Lua nested comments
+data Paren :: * where
+  Sq      :: Paren -- [ ]
+  deriving (Show, Eq)
+
+data Direction = L | R deriving Show -- left / right (open / close)
+
+readBracket :: String -> Maybe Token
+readBracket "[" = Just $ Bracket L Sq
+readBracket "]" = Just $ Bracket R Sq
+readBracket _ = Nothing
+
+readSymbol :: String -> Maybe Token
 readSymbol str = let parses = map ($ str) parseTypes
                in  case filter isJust parses of
-                     (p:_) -> p
-                     []    -> Nothing
+                     (Just p : _) -> Just $ Tok p
+                     []    -> readBracket str
